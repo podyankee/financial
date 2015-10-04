57c2254ca6a9ef970724690eace41334d67c1f23
podyankee <you@gmail.com>
2015-10-04 21:43:02 +0300

    Section Преимущества

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -56,3 +56,17 @@ $(".section_1 .section-content .info-item").equalHeights();
 	$(".s1-bottom .info-item").equalHeights();
 	$(".s2-item").equalHeights();
 	$(".s2-item .img-wrap").equalHeights();
+	//$(".cards .card").equalHeights();
+
+$(".section_4").waypoint(function  () {
+  $(".section_4 .card").each(function  (index) {
+	  var ths = $(this);
+	  setInterval(function  () {
+	    ths.removeClass("card-off").addClass("card-on");
+	  }, 150*index);
+    
+  });
+  
+}, {
+	offset: "5%"
+}); 
